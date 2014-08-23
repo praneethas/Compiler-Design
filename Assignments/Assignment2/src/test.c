79cbdd5ec111d0b162e8a7c0b1b370cb6f4599c9
Praneeth A S <praneethas@ubuntu.ubuntu-domain>
2014-08-23 12:02:27 +0530

    Assignment2

---
+++ Assignments/Assignment2/src/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int main()
+{
+	char* ram="hi";
+	char* sita="hi";
+	char c='?';
+	float = 3.25e5;
+	int car=245;
+	car=246;
+	printf("c = %c\n",c);
+	printf("fib(5) = %d\n",fib(5));
+	return 0;
+}
